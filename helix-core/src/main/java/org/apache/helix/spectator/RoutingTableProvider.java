9c2589741a5c968943e4da4d38308e743f6eeda7
Jiajun Wang <jjwang@linkedin.com>
2018-08-09 22:47:28 -0700

    Fix the monitor reporting thread pool init order in RoutingTableProvider.
    The reporting thread pool was init after listener registers. This may cause some warning message printed because
    of the monitor event fails to be recorded.
    
    RB=1387594
    G=helix-reviewers
    A=hrzhang

--- helix-core/src/main/java/org/apache/helix/spectator/RoutingTableProvider.java
+++ helix-core/src/main/java/org/apache/helix/spectator/RoutingTableProvider.java
@@ -120,6 +120,7 @@ public class RoutingTableProvider
     } catch (JMException e) {
       logger.error("Failed to register RoutingTableProvider monitor MBean.", e);
     }
+    _reportExecutor = Executors.newSingleThreadExecutor();
 
     _routerUpdater = new RouterUpdater(clusterName, _sourceDataType);
     _routerUpdater.start();
@@ -198,8 +199,6 @@ public class RoutingTableProvider
     } else {
       _isPeriodicRefreshEnabled = false;
     }
-
-    _reportExecutor = Executors.newSingleThreadExecutor();
   }
 
   /**
